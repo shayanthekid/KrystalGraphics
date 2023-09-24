537800c49b1ceb73b7d32918b791506689c44a94
shayanthekid <sajidfayazhaniff@gmail.com>
2023-09-24 12:11:26 +0530

    about us done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@ use App\View\Components\ProductCarousel;
         <h2 class="text-2xl font-semibold mb-8 ml-2">More Services</h2>
 
         <!-- Cards Section -->
-        <div class="flex flex-wrap justify-center">
+        <div class="flex flex-wrap justify-center items-center lg:justify-center">
            
             <div class="w-60 mb-8 md:mb-0 md:w-1/4">
                 <div class="bg-gray-100 rounded-lg shadow-lg w-60 h-72 flex justify-center items-center">
@@ -76,6 +76,36 @@ use App\View\Components\ProductCarousel;
                 <h3 class="text-xl font-semibold mt-4">Something</h3>
             </div>
       
+        </div>
+    </div>
+</div>
+
+<!-- About us -->
+<div class="py-16 bg-[#E4E4E4]">
+    <div class="container mx-auto px-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <!-- First Column -->
+        <div>
+            <h2 class="text-3xl font-semibold mb-4 text-left lg:text-4xl lg:w-9/12">Krystal Graphics: Your Printing Partner for Over <span class="text-red-600">35</span> Years!</h2>
+            <p class="text-gray-700 leading-relaxed text-left mb-4 lg:text-xl lg:w-9/12">
+                For more than three decades, Kristal Graphics has been dedicated to delivering top-tier printing services to our valued clients. With an unwavering commitment to excellence, we've established ourselves as a trusted name in the industry.
+            </p>
+            <!-- Add your image/icon here -->
+        </div>
+        
+        <!-- Second Column -->
+        <div>
+            <h3 class="text-lg font-semibold text-gray-600 mb-4 text-left"><span style="color: #ED1F24;">Advance</span> Printing Solutions</h3>
+            <p class="text-gray-700 leading-relaxed text-left mb-4">
+                Our extensive array of printing solutions, from digital and offset to large format, ensures we're equipped with cutting-edge technology to bring your visions to life. Our team comprises adept designers, printers, and customer service champions, all driven by the goal of providing exceptional service.
+            </p>
+            <h3 class="text-lg font-semibold text-gray-600 mb-4 text-left">Crafting with <span style="color: #ED1F24;">Care</span></h3>
+            <p class="text-gray-700 leading-relaxed text-left mb-4">
+                Understanding the uniqueness of each client's needs, we collaborate closely to tailor our offerings to your exact specifications. Whether it's business cards, brochures, flyers, banners, or any print material, we're your one-stop destination.
+            </p>
+            <h3 class="text-lg font-semibold text-gray-600 mb-4 text-left">Your Vision, <span style="color: #ED1F24;">Our Mission</span></h3>
+            <p class="text-gray-700 leading-relaxed text-left">
+                Our mission is clear: to make high-quality printing accessible to all, regardless of budget. We believe in professional printing services that truly empower your ideas. Your trust in Kristal Graphics is the cornerstone of our success, and we're excited to embark on a printing journey that brings your visions to vibrant life.
+            </p>
         </div>
     </div>
 </div>
