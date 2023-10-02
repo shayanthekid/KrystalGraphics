b1c9ef874cfd6f9cf5194641ff486bc15d142386
shayanthekid <sajidfayazhaniff@gmail.com>
2023-10-02 13:53:20 +0530

    card fixed, mobile issue

--- resources/views/components/product-carousel.blade.php
+++ resources/views/components/product-carousel.blade.php
@@ -3,37 +3,79 @@
         <h2 class="text-2xl font-semibold mb-8 ml-2">{{ $title }}</h2>
 
         <!-- Carousel of Cards -->
-        <div class="flex space-x-4 lg:space-x-20 overflow-x-scroll  scrollbar-red">
-            @foreach(json_decode($products) as $product)
-            <div class="w-60">
-                <a href="{{ $product->url }}">
-                    <div class="bg-gray-100 rounded-lg shadow-lg w-60 h-72 flex justify-center items-center">
-                        <img src="{{ asset($product->image) }}" alt="{{ $product->title }}" class="w-36 h-36 object-contain object-center" />
+        <div class="flex items-center justify-center w-full h-full py-24 sm:py-8 px-4">
+            <div class="w-full relative flex items-center justify-center">
+            <button aria-label="slide backward" class="absolute z-30 left-0 ml-10 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="prev">
+    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-white">
+        <svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 1L1 7L7 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </div>
+</button>
+
+                <div class="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
+                    <div id="slider" class="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
+                        @foreach(json_decode($products) as $product)
+                        <div class="flex flex-shrink-0 relative w-full sm:w-auto no-scrollbar ">
+                            <div class="flex space-x-4 lg:space-x-20 overflow-x-scroll no-scrollbar">
+                                <div class="w-60">
+                                    <a href="{{ $product->url }}">
+                                        <div class="bg-gray-100 rounded-lg shadow-lg w-60 h-72 flex justify-center items-center">
+                                            <img src="{{ asset($product->image) }}" alt="{{ $product->title }}" class="w-36 h-36 object-contain object-center" />
+                                        </div>
+                                        <h3 class="text-xl font-semibold mt-4 pb-8">{{ $product->title }}</h3>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
-                    <h3 class="text-xl font-semibold mt-4 pb-8">{{ $product->title }}</h3>
-                </a>
+                </div>
+               <button aria-label="slide forward" class="absolute z-30 right-0 mr-10 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer" id="next">
+    <div class="w-10 h-10 flex items-center justify-center rounded-full bg-white">
+        <svg class="dark:text-gray-900" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L7 7L1 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </div>
+</button>
+
             </div>
-            @endforeach
         </div>
     </div>
 </div>
 
 <style>
- .scrollbar-red::-webkit-scrollbar {
-    width: 6px  !important; /* Adjust the width to make it thinner */
+    .no-scrollbar::-webkit-scrollbar {
+        display: none;
+    }
 
-}
-
-.scrollbar-red::-webkit-scrollbar-thumb {
-    background-color: #F5F5F5;
-    border-radius: 10px;
- height: 2px;
-}
-
-/* .scrollbar-red::-webkit-scrollbar-track {
-    background-color: rgba(0, 0, 0, 0.05);
-} */
-
-/* Push the scrollbar down */
-
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .no-scrollbar {
+        -ms-overflow-style: none; /* IE and Edge */
+        scrollbar-width: none; /* Firefox */
+    }
 </style>
+
+<script>
+    let defaultTransform = 0;
+
+    function goNext() {
+        defaultTransform = defaultTransform - 398;
+        var slider = document.getElementById("slider");
+        if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+        slider.style.transform = "translateX(" + defaultTransform + "px)";
+    }
+
+    const next = document.getElementById("next");
+    next.addEventListener("click", goNext);
+
+    function goPrev() {
+        var slider = document.getElementById("slider");
+        if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+        else defaultTransform = defaultTransform + 398;
+        slider.style.transform = "translateX(" + defaultTransform + "px)";
+    }
+
+    const prev = document.getElementById("prev");
+    prev.addEventListener("click", goPrev);
+</script>
