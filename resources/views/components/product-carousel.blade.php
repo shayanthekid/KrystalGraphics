6738eb79835e099aa3f85dfda665cb22b9508cab
shayanthekid <sajidfayazhaniff@gmail.com>
2023-10-02 14:15:14 +0530

    card issue fixed

--- resources/views/components/product-carousel.blade.php
+++ resources/views/components/product-carousel.blade.php
@@ -1,3 +1,29 @@
+  <div class="lg:hidden">
+        <!-- This will be displayed on smaller screens (mobile view) -->
+     <div class="py-16 bg-white">
+    <div class="container mx-auto">
+        <h2 class="text-2xl font-semibold mb-8 ml-2">{{ $title }}</h2>
+
+        <!-- Carousel of Cards -->
+        <div class="flex space-x-4 lg:space-x-20 overflow-x-scroll  scrollbar-red">
+            @foreach(json_decode($products) as $product)
+            <div class="w-60">
+                <a href="{{ $product->url }}">
+                    <div class="bg-gray-100 rounded-lg shadow-lg w-60 h-72 flex justify-center items-center">
+                        <img src="{{ asset($product->image) }}" alt="{{ $product->title }}" class="w-36 h-36 object-contain object-center" />
+                    </div>
+                    <h3 class="text-xl font-semibold mt-4 pb-8">{{ $product->title }}</h3>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+    </div>
+    <div class="hidden lg:block">
+        <!-- This will be displayed on larger screens (not mobile view) -->
+        
+
 <div class="py-16 bg-white">
     <div class="container mx-auto">
         <h2 class="text-2xl font-semibold mb-8 ml-2">{{ $title }}</h2>
@@ -16,7 +42,7 @@
                 <div class="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
                     <div id="slider" class="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700">
                         @foreach(json_decode($products) as $product)
-                        <div class="flex flex-shrink-0 relative w-full sm:w-auto no-scrollbar ">
+                        <div class="flex flex-shrink-0 relative w-full sm:w-auto no-scrollbar">
                             <div class="flex space-x-4 lg:space-x-20 overflow-x-scroll no-scrollbar">
                                 <div class="w-60">
                                     <a href="{{ $product->url }}">
@@ -43,6 +69,8 @@
         </div>
     </div>
 </div>
+    </div>
+
 
 <style>
     .no-scrollbar::-webkit-scrollbar {
